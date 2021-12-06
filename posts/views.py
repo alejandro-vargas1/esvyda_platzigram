2b67da2d8100f99471b2d0f44210f5215c4bd9d8
xmaugs <alejandro.vargas333@gmail.com>
2021-12-06 15:56:54 -0500

    La M en el MTV modelos en la base de datos y migraciones

--- posts/views.py
+++ posts/views.py
@@ -2,35 +2,42 @@
 
 #import django
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 #import utilities
 from datetime import datetime
 
 posts = [
     {
-        'name':'Mont Blanc',
-        'user':'yesica cortez',
-        'timestamp':datetime.now().strftime('%dth %b, %Y - %H:%M hrs'),
-        'pictures':'https://picsum.photos/200/200/?image=1036'
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/600?image=1036',
     },
     {
-        'name':'Via lactea',
-        'user':'C. Vander',
-        'timestamp':datetime.now().strftime('%dth %b, %Y - %H:%M hrs'),
-        'pictures':'https://picsum.photos/200/200/?image=903'
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/800/?image=903',
+    },
+    {
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/500/700/?image=1076',
     }
 ]
 
 # Create your views here.
 def list_posts(request):
     #List existing posts
-    content = []
-    for post in posts:
-        content.append("""
-            <p><strong>{name}</strong></p>
-            <p><small>{user} - <i>{timestamp}</i></small></p>
-            <figure><img src="{pictures}"></figure>
-        """.format(**post))
-
-    return HttpResponse('<br>'.join(content))
+    return render(request,'feed.html', {'posts':posts})
